888a68d445348f87ef7d633d823c78826fdd568c
flenkbm <flenkbm@gmail.com>
2026-08-13 14:21:12 +0300

    Update scanner.js

--- js/scanner.js
+++ js/scanner.js
@@ -15,17 +15,20 @@ window.addEventListener('load', () => {
 async function resultProcess(res) {
     qrScanner.stop()
     console.log(res);
+    res = res.data;
     if (!/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(res)) {
         qrScanner.start();
+        console.log("code failed");
         return;
     }
+    console.log("trying verifying")
     window.fetch(API+`scan?code=${res}&uuid=${UUID}`)
     .then((response) => {
         return response.json();
     })
     .then((json) => {
         console.log(json);
-        if (json == "-1") {
+        if (json == "-1" || json == "-2") {
             qrScanner.start();
             return;
         }
